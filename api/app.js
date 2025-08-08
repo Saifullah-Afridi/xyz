65b50ab7f10fcb9d6fdf8e91625ba667a6616573
Saif Ullah <saifullahafridi08@gmail.com>
2025-08-08 23:22:41 +0500

    started api

--- api/app.js
+++ api/app.js
@@ -1,8 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 const app = express();
+import cookieParser from "cookie-parser";
 
 app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [],
+    methods: ["GET", "POST", "UPDATE", "DELETE"],
+    Credential: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("welcome to our website");
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ dotenv.config({ path: "./.env" });
 import app from "./app.js";
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
   console.log(`The server is listening on port ${PORT}`);
 });
